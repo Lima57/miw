5a96c8d65203e1c9f59006a417c202c6b3225796
Guilherme <guilhermea.silva.55@gmail.com>
2024-12-16 14:47:00 -0400

    organização de arquivos duplicados

--- main.py
+++ main.py
@@ -4,14 +4,9 @@ Alunos: Kaio de Lima, Marcos Vinnicius, Guilherme Albuquerque, Vitória Shailia
 Professora: Camila Serrão
 '''
 from cadastro import logon
-from login import Acess
 import classes
 
-oficinagenerica = classes.Oficina('Título', 10)
-competicaogenerica = classes.Competicao('Título', 10)
-eventoON = [classes.Evento('Orgulho Nerd', 14, 15, 14112024, oficinagenerica, competicaogenerica), classes.Evento('Evento de Pokémon', 16, 17, 14112024, True, False)]
-oficinas = [classes.Oficina('Oficina de Programação', 40), classes.Oficina('Oficina de Cultura Pop', 40)]
-competicoes = [classes.Competicao('Competição de Cosplay', 15), classes.Competicao('Competição Pokémon TCG', 40)]
+eventos = [classes.Evento('Evento de UNO', 14, 15, 14112024,"sala 10", True, False), classes.Evento('Evento de Pokémon', 16, 17, 14112024,'sala 11', True, False)]
 
 def menu():
     permission = None
@@ -24,7 +19,21 @@ def menu():
 ''')
             choice = input('R:')
             if choice == '1' or 'LOGIN' in choice.upper().split():
-                permission = Acess()
+                with open('DB.txt', 'r') as arquivo:
+
+                    user = input('digite seu usuário:')
+                    keyworld = input('digite sua senha:')
+                    
+                    lines = arquivo.readlines()
+
+                    for line in lines:
+                        if user in line and keyworld in line:
+                            permission = True
+                            print('Login efetuado com sucesso\n')
+                            continue
+                    else:            
+                        print('Login incorreto\n')
+
             elif choice == '2' or 'CADASTRO' in choice.upper().split():
                 logon()
             elif choice == '3' or 'ENCERRAR' in choice.upper().split():
@@ -37,11 +46,9 @@ def menu():
 1 - Cadastro
 2 - Sair de conta
 3 - Encerrar sessão
-4 - Ver oficinas
-5 - Ver competições
-6 - Inscrição de evento
-7 - Inscrição de oficina
-8 - Inscrição de competição
+4 - Ver eventos
+5 - Inscrição de evento
+6 - Gerenciar evento
 ''')
             choice = input('R:')
             if choice == '1' or 'CADASTRO' in choice.upper().split():
@@ -50,51 +57,52 @@ def menu():
                 permission = False
             elif choice == '3' or 'ENCERRAR SESSÃO' in choice.upper().split():
                 break
-            elif choice == '4' or 'VER OFICINAS' in choice.upper().split():
-                for object in range(len(oficinas)):
-                    oficinas[object].getDescricaoOfic()
-                    input('aperte enter para continuar')
-            elif choice == '5' or 'VER COMPETIÇÕES' in choice.upper().split():
-                for object in range(len(competicoes)):
-                    competicoes[object].getDescricaoComp()
-                    input('aperte enter para continuar')
-
-            elif choice == '6' or 'INSCRIÇÃO DE EVENTO' in choice.upper().split():
+            elif choice == '4' or 'VER EVENTOS' in choice.upper().split():
+                for object in range(len(eventos)):
+                    eventos[object].getEvento()
+                    input('aperte enter ara continuar')
+                
+            elif choice == '5' or 'INSCRIÇÃO DE EVENTO' in choice.upper().split():
                 print('Escolha o evento de inscrição:')
-                for object in range(len(eventoON)):
-                    print(f'{object + 1} - {eventoON[object].getTitulo()}')
-
+                for object in range(len(eventos)):
+                    print(f'{object + 1} - {eventos[object].getTitulo()}')
+                
                 inscri = int(input('numero do evento:'))
 
-                if inscri <= len(eventoON):
-                    eventoON[inscri - 1].addParticipante(permission)
+                if 0 <= inscri <= len(eventos):                
+                    eventos[inscri - 1].addParticipante(permission)
                     print('Participante Cadastrado')
                 else:
                     print('\n Número inválido')
 
-            elif choice == '7' or 'INSCRIÇÃO DE OFICINA' in choice.upper().split():
-                print('Escolha a oficina em que irá se inscrever:')
-                for object in range(len(oficinas)):
-                    print(f'{object + 1} - {oficinas[object].getDescricaoOfic()}')
+            elif choice == '6' or 'GERENCIAR EVENTO' in choice.upper().split():
+                print('faça o login como Admin para gerenciar evento (há por padrão um adm salvo no cod): ')
+                with open('DB.txt', 'r') as arquivo:
 
-                inscriofi = int(input('numero do evento:'))
+                    user = input('digite o usuário:')
+                    keyworld = input('digite a senha:')
+                    
+                    lines = arquivo.readlines()
 
-                if inscriofi <= len(oficinas):
-                    oficinas[inscriofi - 1].addParticipanteOfic(permission)
-                    print('Participante Cadastrado')
-                else:
-                    print('\n Número inválido')
+                    for line in lines:
+                        if user in line and keyworld in line:
+                            print('deseja editar qual evento?')
+                            for object in range(len(eventos)):
+                                print(f'{object + 1} - {eventos[object].getTitulo()}')
+                                
+                                escolha = int(input("Escolha o número do evento para gerenciar: ")) - 1
+                                if 0 <= escolha < len(eventos):
+                                    evento_selecionado = eventos[escolha]
+                                    # Chama o método gerenciamentoEvento do evento selecionado
+                                    titulo = input('Novo Título:')
+                                    horarioI = input('Novo Horario de Inicio:')
+                                    horarioF = input('Novo Horario Final:')
+                                    data = input('Nova data:')
 
-            elif choice == '8' or 'INSCRIÇÃO DE COMPETIÇÃO' in choice.upper().split():
-                print('Escolha a competição em que irá se inscrever:')
-                for object in range(len(competicoes)):
-                    print(f'{object + 1} - {competicoes[object].getDescricaoComp()}')
+                                    evento_selecionado.gerenciamentoEvento(evento_selecionado,titulo, horarioI, horarioF, data)
 
-                inscricom = int(input('numero do evento:'))
-
-                if inscricom <= len(competicoes):
-                    competicoes[inscricom - 1].addParticipanteComp(permission)
-                    print('Participante Cadastrado')
-                else:
-                    print('\n Número inválido')
+                                    print('evento gerenciado com sucesso!')
+                                else:
+                                    print("Opção inválida.")
+                                            
 menu()
